733785c603d39432fa315b18a2004293f3bbf7eb
harshita-200 <harshitakedia29@gmail.com>
2023-12-03 23:25:50 +0530

    changes

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ var options={
     loop:true
 };
     var typed= new Typed('.text',options);
+
+const toggle=document.querySelector('#checkbox')
+toggle.addEventListener('change',() =>{
+    if(toggle.checked){
+        document.body.classList.add('dark-theme')
+    }
+    else{
+        document.body.classList.remove('dark-theme')
+    }
+})
